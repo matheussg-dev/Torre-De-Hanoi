edff9027fbe06fa1a9901fb051dd66d81db57a17
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-04-03 15:13:31 -0300

    correcao

--- Torre de hanoi.c
+++ Torre de hanoi.c
@@ -8,14 +8,16 @@ void movetorre(int Numero, char origem, char auxiliar, char destino) {
     // Mover os discos
 
     if (Numero == 1) {
-        printf("Mova o disco do pilar [%c] para o pilar [%c] na jogada %f\n", origem, destino,);
+
+        printf("Mova o disco do pilar [%c] para o pilar [%c].\n", origem, destino);
+        
     } else {
         
-        movetorre(Numero - 1, origem, auxiliar, destino, movimentos);
-        printf("Mova o disco do pilar [%c] para o pilar [%c] na jogada %f\n", origem, destino);
+        movetorre(Numero - 1, origem, auxiliar, destino);
+        printf("Mova o disco do pilar [%c] para o pilar [%c].\n", origem, destino);
         
-        movetorre(Numero - 1, auxiliar, destino, origem, movimentos);
-        printf("Mova o disco do pilar [%c] para o pilar [%c] na jogada %f\n", origem, destino);
+        movetorre(Numero - 1, auxiliar, destino, origem);
+        printf("Mova o disco do pilar [%c] para o pilar [%c].\n", origem, destino);
         
     }
 }
@@ -26,7 +28,7 @@ int main() {
 
     int numerosDeDiscos = 3;
 
-    movetorre(numerosDeDiscos, 'A', 'B', 'C',);
+    movetorre(numerosDeDiscos, 'A', 'B', 'C');
     
     return 0;
 }
