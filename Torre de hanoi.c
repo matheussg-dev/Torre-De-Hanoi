5c18723fae1573e6a64264ccb8b990dda6818c4e
CAS489\matheus.gordilho <matheus.gordilho@castecnologia.com.br>
2024-04-04 12:11:32 -0300

    teste 2

--- Torre de hanoi.c
+++ Torre de hanoi.c
@@ -3,21 +3,22 @@
 // Função para resolver a Torre de Hanoi as 3 pilastras
 // Objetivo mover todos os Discos para pilastra oposta
 
-void movetorre(int Numero, char origem, char auxiliar, char destino) {
+void movetorre(int Numero, char origem, char auxiliar, char destino, int movimentos) {
     
     // Mover os discos
 
     if (Numero == 1) {
-
-        printf("Mova o disco do pilar [%c] para o pilar [%c].\n", origem, destino);
-        
+        printf("Mova o disco do pilar [%c] para o pilar [%c]\n", origem, destino);
+        movimentos++;
     } else {
         
         movetorre(Numero - 1, origem, auxiliar, destino);
-        printf("Mova o disco do pilar [%c] para o pilar [%c].\n", origem, destino);
+        printf("Mova o disco do pilar [%c] para o pilar [%c]\n", origem, destino);
+        movimentos++;
         
         movetorre(Numero - 1, auxiliar, destino, origem);
-        printf("Mova o disco do pilar [%c] para o pilar [%c].\n", origem, destino);
+        printf("Mova o disco do pilar [%c] para o pilar [%c]\n", origem, destino);
+        movimentos++;
         
     }
 }
@@ -27,8 +28,11 @@ int main() {
     // Número de discos
 
     int numerosDeDiscos = 3;
+    int movimentos = 0;
 
-    movetorre(numerosDeDiscos, 'A', 'B', 'C');
+    movetorre(numerosDeDiscos, 'A', 'B', 'C', movimentos);
+    
+    printf("\nTotal de movimentos: %d\n", movimentos);
     
     return 0;
 }
